f61c785d4e320f89bde35500d748e9608f60cecf
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-06-08 21:07:28 +0900

    サニタイズとHTML Parseの基本的なものを持って来た

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,21 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.request_and_parse_html(url, method = :get)
+    http_client = HTTPClient.new
+    response = http_client.send(method, url, {}, {})
+    doc = Nokogiri::HTML.parse(response.body)
+    return doc
+  end
+
+  def self.basic_sanitize(text)
+    #絵文字を除去
+    sanitized_word = text.encode('SJIS', 'UTF-8', invalid: :replace, undef: :replace, replace: '').encode('UTF-8')
+    #全角半角をいい感じに整える
+    sanitized_word = Charwidth.normalize(sanitized_word)
+    
+    # 余分な空欄を除去
+    sanitized_word.strip!
+    return sanitized_word
+  end
 end
