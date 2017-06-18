fff8ddb561edbe1190ed73997bf04937c8c6ac86
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-06-19 01:46:29 +0900

    serverを起動するので設定をいじった

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,27 @@
+before_fork do
+  PumaWorkerKiller.config do |config|
+    config.ram           = 2048
+    config.frequency     = 5
+    config.percent_usage = 0.8
+    config.rolling_restart_frequency = 12 * 3600
+    config.reaper_status_logs = true
+  end
+  PumaWorkerKiller.start
+end
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
 #
+
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3200 }
 
 # Specifies the `environment` that Puma will run in.
 #
