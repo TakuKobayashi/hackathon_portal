1a91b7a065f201142312a01e8b278e9c3588998b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-07-18 14:32:43 +0900

    stop springを追加しておいた

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ RAILS_ENV=production rails db:migrate
 RAILS_ENV=production rails assets:clean
 RAILS_ENV=production rails assets:precompile
 whenever --update-crontab
+spring stop
 kill -9 `cat tmp/pids/server.pid`
 SECRET_KEY_BASE=$(rake secret) rails server -e production -p 3200 -d
