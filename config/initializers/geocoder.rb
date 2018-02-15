7582dcbe0afe60a0f0880843d29d0bfc343d8bd1
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-02-15 14:13:50 +0900

    なんかエラル

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,6 @@
 api_config = YAML.load(File.read("#{Rails.root.to_s}/config/apiconfig.yml"))
 Geocoder.configure(
   api_key: api_config["google"]["apikey"],
-  language: "ja"
+  language: "ja",
+  use_https: => true
 )
