782ae245678265469c431211802ff8fee7e42168
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-05-04 00:32:17 +0900

    Request関係を更新

--- app/models/concerns/request_parser.rb
+++ app/models/concerns/request_parser.rb
@@ -2,13 +2,20 @@ require 'xmlsimple'
 
 module RequestParser
   def self.request_and_parse_html(url: ,method: :get, params: {}, header: {}, options: {})
-    text = self.request_and_response_body_text(url: url,method: method, params: params, header: header, options: options)
+    text = self.request_and_response_body(url: url,method: method, params: params, header: header, options: options)
     doc = Nokogiri::HTML.parse(text)
     return doc
   end
 
+  def self.request_and_get_links_from_html(url:, method: :get, params: {}, header: {}, options: {})
+    doc = self.request_and_parse_html(url: url,method: method, params: params, header: header, options: options)
+    result = {}
+    doc.css('a').select{|anchor| anchor[:href].present? && anchor[:href] != "/" }.each{|anchor| result[anchor[:href]] = anchor.text }
+    return result
+  end
+
   def self.request_and_parse_json(url: ,method: :get, params: {}, header: {}, options: {})
-    text = self.request_and_response_body_text(url: url,method: method, params: params, header: header, options: options)
+    text = self.request_and_response_body(url: url,method: method, params: params, header: header, options: options)
     parsed_json = {}
     begin
       parsed_json = JSON.parse(text)
@@ -19,12 +26,12 @@ module RequestParser
   end
 
   def self.request_and_parse_xml(url: ,method: :get, params: {}, header: {}, options: {})
-    text = self.request_and_response_body_text(url: url,method: method, params: params, header: header, options: options)
+    text = self.request_and_response_body(url: url,method: method, params: params, header: header, options: options)
     parsed_xml = XmlSimple.xml_in(text)
     return parsed_xml
   end
 
-  def self.request_and_response_body_text(url: ,method: :get, params: {}, header: {}, options: {})
+  def self.request_and_response_body(url: ,method: :get, params: {}, header: {}, options: {})
     http_client = HTTPClient.new
     http_client.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_NONE
     http_client.connect_timeout = 600
@@ -37,7 +44,7 @@ module RequestParser
         self.record_log(url: url, method: method, params: params, header: header, options: options, insert_top_messages: ["request Error Status Code: #{response.status}"])
       end
       result = response.body
-    rescue SocketError => e
+    rescue SocketError, HTTPClient::ConnectTimeoutError, HTTPClient::BadResponseError, Addressable::URI::InvalidURIError => e
       self.record_log(url: url, method: method, params: params, header: header, options: options, error_messages: ["error: #{e.message}"] + e.backtrace, insert_top_messages: ["exception:" + e.class.to_s])
     end
     return result
