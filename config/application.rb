7da67727b3c681623578418e452c505ffc4fc54a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-10-14 03:50:55 +0900

    rails 5.2.1にバージョンアップ

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,8 @@ Bundler.require(*Rails.groups)
 
 module HackathonPortal
   class Application < Rails::Application
-    config.load_defaults 5.1
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
 
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
@@ -18,7 +19,8 @@ module HackathonPortal
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV.fetch('TWITTER_CONSUMER_KEY', ''), ENV.fetch('TWITTER_CONSUMER_SECRET', '')
-end
