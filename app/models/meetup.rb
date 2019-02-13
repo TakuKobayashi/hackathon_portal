54b5b4263aa9b48d84016dc5be21dd63a80a8d24
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-02-13 13:08:12 +0900

    Commonとして吐き出して、Unityにも一部適用させた

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -64,7 +64,7 @@ class Meetup < Event
         vanue_hash = res["venue"] || {}
         fee_hash = res["fee"] || {}
         group_hash = res["group"] || {}
-        meetup_event.attributes = meetup_event.attributes.merge({
+        meetup_event.attributes = meetup_event.attributes.merge_attributes_and_set_location_data(attrs: {
           title: Sanitizer.basic_sanitize(res["name"].to_s),
           url: res["link"].to_s,
           description: Sanitizer.basic_sanitize(res["description"].to_s),
@@ -84,7 +84,6 @@ class Meetup < Event
           attend_number: res["yes_rsvp_count"] || res["attendance_count"],
           substitute_number: res["waitlist_count"]
         })
-        meetup_event.set_location_data
         meetup_event.save!
         meetup_event.import_hashtags!(hashtag_strings: meetup_event.search_hashtags)
       end
--- app/models/scaling/unity_event.rb
+++ app/models/scaling/unity_event.rb
@@ -36,4 +36,10 @@
 #
 
 class Scaling::UnityEvent < ApplicationRecord
+  include EventCommon
+
+  has_many :summaries, as: :resource, class_name: 'Ai::ResourceSummary'
+  has_many :resource_hashtags, as: :resource, class_name: 'Ai::ResourceHashtag'
+  has_many :hashtags, through: :resource_hashtags, source: :hashtag
+  accepts_nested_attributes_for :hashtags
 end
