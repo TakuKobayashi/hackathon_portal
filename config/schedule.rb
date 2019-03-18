4d496ed2362f4829af62f22aa330ff184a125226
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-03-18 15:16:57 +0900

    update schesule and refactor

--- config/schedule.rb
+++ config/schedule.rb
@@ -28,14 +28,22 @@ set :output, "#{path}/log/cron.log"
 #  job_type :script,  %{cd :path && :environment_variable=:environment :rbenv_root/bin/rbenv exec bundle exec script/:task :output}
 #end
 
-every :day, at: '2:00' do
+every :day, at: '10:00' do
   rake "batch:event_crawl"
 end
 
-every :day, at: '10:00' do
+every :day, at: '15:00' do
+  rake "backup:update_table_to_spreadsheet"
+end
+
+every :day, at: '19:00' do
   rake "batch:bot_tweet"
 end
 
-#every 3.hours do
-#  runner "Ai::TwitterResource.crawl_hashtag_tweets!"
-#end
+every :day, at: '22:00' do
+  rake "backup:dump_and_upload_and_clear_data"
+end
+
+every 3.hours do
+  runner "Ai::TwitterResource.crawl_hashtag_tweets!"
+end
