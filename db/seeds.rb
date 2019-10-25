7911a0771c40184ef578718e5798d75466c11838
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-10-25 12:24:42 +0900

    seedsにSQLファイルをぶっこんでいく方法に変更するのでseedでデータを取得できるようにする

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sql_files = Dir.glob(Rails.root.join("db", "seeds", "*.sql"))
+sql_files.each do |sql_file|
+  sql = File.open(sql_file) { |f| f.read }
+  # split multiple queries
+  queries = sql.split(/;$/)
+  # remove last blank line if exists
+  queries.pop if queries[-1] =~ /^\s+$/
+
+  ActiveRecord::Base.transaction do
+    queries.each do |q|
+      result = ActiveRecord::Base.connection.execute(q)
+    end
+  end
+end
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
