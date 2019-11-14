e27a2c432a6452c7ae9474d8fa475df61a5347f2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-11-14 19:27:51 +0900

    update_allにしたらいいのでは?

--- db/migrate/20191030054129_extends_columns_type_to_informed_from.rb
+++ db/migrate/20191030054129_extends_columns_type_to_informed_from.rb
@@ -3,48 +3,17 @@ class ExtendsColumnsTypeToInformedFrom < ActiveRecord::Migration[6.0]
     add_column :events, :informed_from, :integer, null: false, default: 0
     add_column :scaling_unity_events, :informed_from, :integer, null: false, default: 0
     [Event, Scaling::UnityEvent].each do |clazz|
-      clazz.find_each do |event|
-        if event.type.include?('Atnd')
-          event.atnd!
-        elsif event.type.include?('Connpass')
-          event.connpass!
-        elsif event.type.include?('Doorkeeper')
-          event.doorkeeper!
-        elsif event.type.include?('Peatix')
-          event.peatix!
-        elsif event.type.include?('Meetup')
-          event.meetup!
-        elsif event.type.include?('GoogleFormEvent')
-          event.google_form!
-        elsif event.type.include?('TwitterEvent')
-          event.twitter!
-        end
-      end
+      clazz.where(type: 'Atnd').update_all(informed_from: clazz.informed_froms[:atnd])
+      clazz.where(type: 'Connpass').update_all(informed_from: clazz.informed_froms[:connpass])
+      clazz.where(type: 'Doorkeeper').update_all(informed_from: clazz.informed_froms[:doorkeeper])
+      clazz.where(type: 'Peatix').update_all(informed_from: clazz.informed_froms[:peatix])
+      clazz.where(type: 'Meetup').update_all(informed_from: clazz.informed_froms[:meetup])
+      clazz.where(type: 'GoogleFormEvent').update_all(informed_from: clazz.informed_froms[:google_form])
+      clazz.where(type: 'TwitterEvent').update_all(informed_from: clazz.informed_froms[:twitter])
     end
   end
 
   def down
-    [Event, Scaling::UnityEvent].each do |clazz|
-      clazz.find_each do |event|
-        if event.atnd?
-          type.update!(type: 'Atnd')
-        elsif event.connpass?
-          type.update!(type: 'Connpass')
-        elsif event.doorkeeper?
-          type.update!(type: 'Doorkeeper')
-        elsif event.peatix?
-          type.update!(type: 'Peatix')
-        elsif event.meetup?
-          type.update!(type: 'Meetup')
-        elsif event.type.google_form?
-          type.update!(type: 'GoogleFormEvent')
-        elsif event.type.twitter?
-          type.update!(type: 'TwitterEvent')
-        else
-          type.update!(type: 'SelfPostEvent')
-        end
-      end
-    end
     remove_column :events, :informed_from
     remove_column :scaling_unity_events, :informed_from
   end
