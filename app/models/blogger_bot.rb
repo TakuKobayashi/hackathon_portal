068745b4b53b245304654c153323e30b146a5b8b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-07-19 12:46:12 +0900

    bugfix

--- app/models/blogger_bot.rb
+++ app/models/blogger_bot.rb
@@ -41,7 +41,7 @@ class BloggerBot < ApplicationRecord
   end
 
   def build_content(action_view_renderer:)
-    before_events, after_events = Event.where(id: blogger_bot.event_ids).order('started_at ASC').partition do |e|
+    before_events, after_events = Event.where(id: self.event_ids).order('started_at ASC').partition do |e|
       e.ended_at.present? ? e.ended_at > Time.current : (e.started_at + 2.day) > Time.current
     end
     start_month = date_number % 10000
