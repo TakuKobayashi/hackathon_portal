124137069183cd196372adaf58a91798d79d122d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-08-09 11:54:15 +0900

    twitter follower増加大作戦

--- app/models/twitter_bot.rb
+++ app/models/twitter_bot.rb
@@ -36,6 +36,49 @@ class TwitterBot < ApplicationRecord
     destroy!
   end
 
+  def self.promote!(access_token: nil, access_token_secret: nil)
+    twitter_client = self.get_twitter_client(access_token: access_token, access_token_secret: access_token_secret)
+    follower_ids = twitter_client.follower_ids({count: 5000})
+    follower_ids.each do |follower_id|
+      csrf_token = SecureRandom.hex
+      json = RequestParser.request_and_parse_json(
+        url: "https://api.twitter.com/2/timeline/liked_by.json",
+        params: {
+          include_profile_interstitial_type: 1,
+          include_blocking: 1,
+          include_blocked_by: 1,
+          include_followed_by: 1,
+          include_want_retweets: 1,
+          include_mute_edge: 1,
+          include_can_dm: 1,
+          include_can_media_tag: 1,
+          skip_status: 1,
+          cards_platform: "Web-12",
+          include_cards: 1,
+          include_ext_alt_text: true,
+          include_quote_count: true,
+          include_reply_count: 1,
+          tweet_mode: "extended",
+          include_entities: true,
+          include_user_entities: true,
+          include_ext_media_color: true,
+          include_ext_media_availability: true,
+          send_error_codes: true,
+          simple_quoted_tweet: true,
+          tweet_id: follower_id,
+          count: 80,
+          ext: 'mediaStats%2ChighlightedLabel',
+        },
+        header: {
+          # auth_token と Bearer token は調べて入れる必要がある
+          "authorization" => ["Bearer", twitter_client.token].join(" "),
+          "x-csrf-token" => csrf_token,
+          'cookie' => ['auth_token=' + csrf_token + ';', 'ct0=' + csrf_token + ';'].join(" ")
+        }
+      )
+    end
+  end
+
   def self.get_twitter_client(
     access_token: ENV.fetch('TWITTER_BOT_ACCESS_TOKEN', ''),
     access_token_secret: ENV.fetch('TWITTER_BOT_ACCESS_TOKEN_SECRET', '')
