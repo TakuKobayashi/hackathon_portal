1b1f7d84a10274d0b9b86d0b4354ccaebd5cb35f
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-08-27 13:01:14 +0900

    width, heightの0の時の対処

--- app/models/concerns/adjust_image.rb
+++ app/models/concerns/adjust_image.rb
@@ -2,11 +2,19 @@ module AdjustImage
   def self.calc_resize_text(width:, height:, max_length:)
     if width > height
       resized_width = [width, max_length].min
-      resized_height = ((resized_width.to_f / width.to_f) * height.to_f).to_i
+      if width == 0
+        resized_height = 0
+      else
+        resized_height = ((resized_width.to_f / width.to_f) * height.to_f).to_i
+      end
       return "#{resized_width.to_i}x#{resized_height.to_i}"
     else
       resized_height = [height, max_length].min
-      resized_width = ((resized_height.to_f / height.to_f) * width.to_f).to_i
+      if height == 0
+        resized_width = 0
+      else
+        resized_width = ((resized_height.to_f / height.to_f) * width.to_f).to_i
+      end
       return "#{resized_width.to_i}x#{resized_height.to_i}"
     end
   end
