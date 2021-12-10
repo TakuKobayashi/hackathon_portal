d52141e5f772d2480b3e187799a6aa24262f07f9
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-12-11 06:26:48 +0900

    devpostをこれまでの共通処理に合わせるようにした
    (cherry picked from commit eb01a895d82ffd4ebf03e75405aa2da799be17fe)

--- app/models/concerns/devpost_operation.rb
+++ app/models/concerns/devpost_operation.rb
@@ -70,28 +70,34 @@ module DevpostOperation
     detail_page = RequestParser.request_and_parse_html(url: url)
     event_json = JSON.parse(detail_page.css('#challenge-json-ld').text.strip)
     event_struct = OpenStruct.new(event_json)
-    devpost_event =
-      Event.new(
-        url: event_struct.url.to_s,
+    devpost_event = Event.new(
+      url: event_struct.url.to_s,
+    )
+    organizer_struct = OpenStruct.new(event_struct.organizer || {})
+    location_struct = OpenStruct.new(event_struct.location || {})
+    address_struct = OpenStruct.new(location_struct.address || {})
+    if address_struct.name.downcase == 'online'
+      event_struct.place = address_struct.name
+    else
+      event_struct.address = address_struct.streetAddress
+      event_struct.place = detail_page.css('.location').css('p').first.try(:text) || address_struct.streetAddress
+      event_struct.place = event_struct.place.to_s.strip
+    end
+    devpost_event.merge_event_attributes(
+      attrs: {
         informed_from: :devpost,
         title: event_struct.name.to_s,
         description: event_struct.description.to_s,
         state: :active,
         started_at: Time.parse(event_struct.startDate),
         ended_at: Time.parse(event_struct.endDate),
-      )
-    organizer_struct = OpenStruct.new(event_struct.organizer || {})
-    location_struct = OpenStruct.new(event_struct.location || {})
-    address_struct = OpenStruct.new(location_struct.address || {})
-    if address_struct.name.downcase == 'online'
-      devpost_event.place = address_struct.name
-    else
-      devpost_event.address = address_struct.streetAddress
-      devpost_event.place = detail_page.css('.location').css('p').first.try(:text) || address_struct.streetAddress
-      devpost_event.place = devpost_event.place.to_s.strip
-    end
-    devpost_event.merge_event_attributes(
-      attrs: { attend_number: 0, max_prize: 0, currency_unit: 'JPY', owner_name: organizer_struct.name }.merge(options),
+        place: event_struct.place,
+        address: event_struct.address,
+        attend_number: 0,
+        max_prize: 0,
+        currency_unit: 'JPY',
+        owner_name: organizer_struct.name
+      }.merge(options),
     )
     return devpost_event
   end
