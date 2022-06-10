e4d1e86bc4036f2c9667e19227c31248fa4d2e4d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-06-11 03:41:59 +0900

    rails 7にあげるための設定

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module HackathonPortal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
