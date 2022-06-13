044600da422ca8e3fbf2d885e2c08cf0ab24192b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-06-13 21:33:17 +0900

    css buildの設定を入れる必要がありそうなので

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
