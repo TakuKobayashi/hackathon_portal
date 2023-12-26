26a0f406d417a3d159c566fbc3098864ab2594ec
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2023-12-26 10:07:55 +0000

    add 2023-12-26-10:07:55 tweet-from-last-update-data

--- db/seeds/event_calendar_bots/00.sql
+++ db/seeds/event_calendar_bots/00.sql
@@ -5887,3 +5887,4 @@ INSERT INTO `event_calendar_bots` (`id`, `from_type`, `from_id`, `calender_event
 INSERT INTO `event_calendar_bots` (`id`, `from_type`, `from_id`, `calender_event_id`, `created_at`, `updated_at`) VALUES (6223,'Event',694771,'1mb4ki2tdke831ulfbo7lkf7ag','2023-12-24 10:07:12','2023-12-24 10:07:12');
 INSERT INTO `event_calendar_bots` (`id`, `from_type`, `from_id`, `calender_event_id`, `created_at`, `updated_at`) VALUES (6224,'Event',694777,'ph6pk7qec8r58i6b3qjflvio3g','2023-12-25 10:06:54','2023-12-25 10:06:54');
 INSERT INTO `event_calendar_bots` (`id`, `from_type`, `from_id`, `calender_event_id`, `created_at`, `updated_at`) VALUES (6225,'Event',694776,'6ol0m71fcr4ipgel06717jem6c','2023-12-25 10:06:55','2023-12-25 10:06:55');
+INSERT INTO `event_calendar_bots` (`id`, `from_type`, `from_id`, `calender_event_id`, `created_at`, `updated_at`) VALUES (6226,'Event',694730,'pivd67t9eqcu5ubat14q8g89qk','2023-12-26 10:07:07','2023-12-26 10:07:07');
