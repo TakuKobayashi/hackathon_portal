bc7d7f529c60687b61071d017728e689d831fbf6
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-02-26 05:00:25 +0000

    add 2024-02-26-05:00:25 crawl-and-publish

--- db/seeds/ai_resource_hashtags/01.sql
+++ db/seeds/ai_resource_hashtags/01.sql
@@ -452,3 +452,5 @@ INSERT INTO `ai_resource_hashtags` (`id`, `resource_type`, `resource_id`, `hasht
 INSERT INTO `ai_resource_hashtags` (`id`, `resource_type`, `resource_id`, `hashtag_id`) VALUES (10534,'Event',695364,3690);
 INSERT INTO `ai_resource_hashtags` (`id`, `resource_type`, `resource_id`, `hashtag_id`) VALUES (10535,'Event',695366,2739);
 INSERT INTO `ai_resource_hashtags` (`id`, `resource_type`, `resource_id`, `hashtag_id`) VALUES (10536,'Event',695368,4498);
+INSERT INTO `ai_resource_hashtags` (`id`, `resource_type`, `resource_id`, `hashtag_id`) VALUES (10537,'Event',695369,3440);
+INSERT INTO `ai_resource_hashtags` (`id`, `resource_type`, `resource_id`, `hashtag_id`) VALUES (10538,'Event',695372,4295);
