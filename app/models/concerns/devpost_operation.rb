77d04ce27c29a5d979eef2063dc99a0ea9d48445
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-08-18 21:26:52 +0900

    devpostからデータを取得する方法が変わったのでその内容の実装方法を適用した
    (cherry picked from commit fbbb8ab2fe28907d74c067ad45ed6f5bdf8fd596)

--- app/models/concerns/devpost_operation.rb
+++ app/models/concerns/devpost_operation.rb
@@ -1,5 +1,7 @@
 module DevpostOperation
   DEVPOST_HACKATHONS_URL = 'https://devpost.com/hackathons'
+  DEVPOST_HACKATHONS_API_URL = 'https://devpost.com/api/hackathons'
+  MAX_PER_PAGE = 40
 
   def self.import_events_from_keywords!(keywords:)
     self.imoport_hackathon_events!
@@ -8,59 +10,57 @@ module DevpostOperation
   def self.imoport_hackathon_events!
     page = 1
     loop do
-      doc =
-        RequestParser.request_and_parse_html(
-          url: DEVPOST_HACKATHONS_URL,
+      response_json =
+        RequestParser.request_and_parse_json(
+          url: DEVPOST_HACKATHONS_API_URL,
           params: {
             page: page,
+            per_page: MAX_PER_PAGE,
+            "status[]" => ["upcoming", "open"],
           },
           options: {
             follow_redirect: true,
           },
         )
-      url_event_options = {}
-      doc
-        .css('article')
-        .each do |article_dom|
-          content_list = article_dom.css('li')
-          a_tag = article_dom.css('a').first || {}
-          a_url = Addressable::URI.parse(a_tag[:href].to_s)
-
-          # query(?以降)は全て空っぽにしておく
-          a_url.query_values = nil
-
-          # fragment(#以降)は全て空っぽにしておく
-          a_url.fragment = nil
-          price_text = content_list[0].try(:css, '.value').try(:text).to_s
-          Rails.logger.info({ url: a_url.to_s, price: price_text })
-          if price_text[0] =~ /[0-9]/
-            currency_unit = 'EUR'
-          elsif price_text[0] == '$'
-            currency_unit = 'USD'
-          else
-            currency_unit = 'INR'
-          end
-          price_number = price_text.scan(/[0-9]/).join.to_i
-          attend_text = content_list[2].try(:css, '.value').try(:text).to_s
-          url_event_options[a_url.to_s] = {
-            'max_prize' => price_number,
-            'currency_unit' => currency_unit,
-            'attend_number' => attend_text.to_i,
-          }
+      hackathon_info_structs = (response_json["hackathons"] || []).map{|info| OpenStruct.new(info) }
+      url_devpost_events = Event.where(url: hackathon_info_structs.map(&:url)).includes(:event_detail).index_by(&:url)
+      hackathon_info_structs.each do |info|
+        devpost_event = url_devpost_events[info.url]
+        if devpost_event.blank?
+          devpost_event = Event.new(url: info.url.to_s)
         end
+        started_at_date, ended_at_date = self.split_to_start_and_end_date(date_range_string: info.submission_period_dates)
+        location_strut = OpenStruct.new(info.displayed_location)
+        address_string = nil
+        if location_strut.location.downcase != 'online'
+          address_string = location_strut.location
+        end
+        price_amount, currency_unit = self.split_price_and_currency_unit(prize_amount_string: info.prize_amount)
+        devpost_event.merge_event_attributes(
+          attrs: {
+            informed_from: :devpost,
+            title: info.title.to_s,
+            state: :active,
+            started_at: started_at_date,
+            ended_at: ended_at_date,
+            place: location_strut.location,
+            address: address_string,
+            attend_number: info.registrations_count.to_i,
+            max_prize: price_amount,
+            currency_unit: currency_unit,
+            owner_name: info.organization_name,
+          },
+        )
 
-      # HTML Parseして1件もなかったらその時点でこれ以上のloopをやめるようにする
-      break if url_event_options.blank?
-      url_devpost_events = Event.where(url: url_event_options.keys).includes(:event_detail).index_by(&:url)
-      url_event_options.keys.each do |event_url|
-        next if url_devpost_events[event_url].present?
-        devpost_event = self.analyze_and_build_event(url: event_url, options: url_event_options[event_url])
         if devpost_event.place.downcase.match(Sanitizer.online_regexp).present? ||
              devpost_event.place.downcase.include?('japan')
+          tags = (info.themes || []).map{|theme| theme["name"]}
           devpost_event.save!
-          devpost_event.import_hashtags!(hashtag_strings: devpost_event.search_hashtags)
+          devpost_event.import_hashtags!(hashtag_strings: tags)
         end
       end
+      meta_struct = OpenStruct.new(response_json["meta"] || {})
+      break if (page * MAX_PER_PAGE) >= meta_struct.total_count.to_i
       page += 1
       sleep 1
     end
@@ -98,5 +98,39 @@ module DevpostOperation
       }.merge(options),
     )
     return devpost_event
+  end
+
+  private
+  def self.split_to_start_and_end_date(date_range_string:)
+    start_date_string, another_date_string = date_range_string.split('-')
+    if another_date_string.present?
+      end_date_string, year_string = another_date_string.split(',')
+      started_at_date = Time.parse([start_date_string.strip, year_string].join(','))
+      if (end_date_string.strip =~ /^[0-9]+$/).nil?
+        ended_at_date = Time.parse([end_date_string.strip, year_string].join(','))
+      else
+        start_month_string, start_day_string = start_date_string.strip.split(' ')
+        ended_at_date = Time.parse([[start_month_string, end_date_string.strip].join(' '), year_string].join(','))
+      end
+    else
+      start_date_string_strip, year_string = start_date_string.strip.split(',')
+      started_at_date = Time.parse([start_date_string_strip, year_string].join(','))
+      ended_at_date = started_at_date.end_of_day
+    end
+    return [started_at_date, ended_at_date]
+  end
+
+  def self.split_price_and_currency_unit(prize_amount_string:)
+    price_doc = Nokogiri::HTML.parse(prize_amount_string)
+    price_currency_string = price_doc.text
+    price_amount = price_currency_string.gsub(/[^\d]/, "").to_i
+    if price_currency_string.start_with?('$CAD')
+      return [price_amount, 'CAD']
+    elsif price_currency_string.start_with?('$')
+      return [price_amount, 'USD']
+    elsif price_currency_string.start_with?('¥')
+      return [price_amount, 'JPY']
+    end
+    return [price_amount, 'USD']
   end
 end
