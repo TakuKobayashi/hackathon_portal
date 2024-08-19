e51cdf76d8b050247a7a79585b9151bd67481288
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-08-19 20:11:29 +0900

    connpassのhtmlから情報を取得する処理の一部を実装した

--- app/models/concerns/connpass_operation.rb
+++ app/models/concerns/connpass_operation.rb
@@ -55,7 +55,53 @@ module ConnpassOperation
         },
       )
       event_list_docs = response_html.css(".event_list")
+      event_urls = event_list_docs.map{|doc| doc.css("a.url").first.try(:attr, "href") }.compact
+      current_url_events = Event.where(url: event_urls).includes(:event_detail).index_by(&:url)
+
       event_list_docs.each do |event_doc|
+        event_url_doc = event_doc.css("a.url").first
+        event_url = Addressable::URI.parse(event_url_doc.try(:attr, "href").to_s)
+        if current_url_events[event_url.href].present?
+          connpass_event = current_url_events[event_url.href]
+        else
+          connpass_event = Event.new(url: event_url.href)
+        end
+        event_id = event_url.path.split("/").find_all(&:present?).last
+        attend_number, limit_number = event_doc.css("span.amount").text.split('/')
+        substitute_number = nil
+        if limit_number.present?
+          substitute_number = [attend_number.to_i - limit_number.to_i, 0].max
+        end
+        detail_page_doc = RequestParser.request_and_parse_html(url: event_url.href)
+=begin
+        connpass_event.merge_event_attributes(
+          attrs: {
+            state: :active,
+            informed_from: :connpass,
+            event_id: event_id,
+            title: event_url_doc.try(:text).to_s,
+            description: Sanitizer.basic_sanitize(detail_page_doc.css("#editor_area").children.to_html.strip.to_s),
+            limit_number: limit_number,
+            address: api_response_hash['address'],
+            place: api_response_hash['place'].to_s,
+            lat: api_response_hash['lat'],
+            lon: api_response_hash['lon'],
+            cost: 0,
+            max_prize: 0,
+            currency_unit: 'JPY',
+            owner_id: api_response_hash['owner_id'],
+            owner_nickname: api_response_hash['owner_nickname'],
+            owner_name: api_response_hash['owner_display_name'],
+            attend_number: attend_number.to_i,
+            substitute_number: substitute_number,
+            started_at: api_response_hash['started_at'],
+            ended_at: api_response_hash['ended_at'],
+          },
+        )
+=end
+        connpass_event.save!
+        hashtags = detail_page_doc.css(".label_hashtag").map{|hashtag_dom| hashtag_dom.css("a").map{|a_tag| a_tag.text.strip } }.flatten
+        connpass_event.import_hashtags!(hashtag_strings: hashtags)
       end
     end
   end
