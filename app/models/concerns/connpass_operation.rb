23e104389e3fbc7075c5b5969235dbf3452b3d1e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-08-20 01:09:48 +0900

    connpassのイベント情報を取得できるような処理を作成してみた

--- app/models/concerns/connpass_operation.rb
+++ app/models/concerns/connpass_operation.rb
@@ -51,29 +51,36 @@ module ConnpassOperation
         url: CONNPASS_SEARCH_URL,
         params: {
           q: keyword,
-          start_from: Time.current.strftime("%Y/%m/%d")
+          start_from: Time.current.strftime("%Y/%m/%d"),
         },
       )
       event_list_docs = response_html.css(".event_list")
-      event_urls = event_list_docs.map{|doc| doc.css("a.url").first.try(:attr, "href") }.compact
+      event_urls = event_list_docs.map{|doc| doc.css("a.url").first.try(:attr, 'href') }.compact
       current_url_events = Event.where(url: event_urls).includes(:event_detail).index_by(&:url)
-
       event_list_docs.each do |event_doc|
         event_url_doc = event_doc.css("a.url").first
-        event_url = Addressable::URI.parse(event_url_doc.try(:attr, "href").to_s)
-        if current_url_events[event_url.href].present?
-          connpass_event = current_url_events[event_url.href]
+        event_url = Addressable::URI.parse(event_url_doc.try(:attr, 'href').to_s)
+        if current_url_events[event_url.to_s].present?
+          connpass_event = current_url_events[event_url.to_s]
         else
-          connpass_event = Event.new(url: event_url.href)
+          connpass_event = Event.new(url: event_url.to_s)
         end
-        event_id = event_url.path.split("/").find_all(&:present?).last
+        event_id = event_url.path.split('/').find_all(&:present?).last
         attend_number, limit_number = event_doc.css("span.amount").text.split('/')
         substitute_number = nil
         if limit_number.present?
           substitute_number = [attend_number.to_i - limit_number.to_i, 0].max
         end
-        detail_page_doc = RequestParser.request_and_parse_html(url: event_url.href)
-=begin
+        event_owner_doc = event_doc.css('.event_owner')
+        owner_info_doc = event_owner_doc.css('a').first
+        owner_nickname = owner_info_doc.try(:attr, 'href').split('/').find_all(&:present?).last
+        owner_name = owner_info_doc.try(:text).to_s.strip
+        detail_page_doc = RequestParser.request_and_parse_html(url: event_url.to_s)
+        event_schedule_doc = detail_page_doc.css('.event_schedule_area')
+        start_at_value = event_schedule_doc.css('.dtstart').css('.value-title').first.try(:attr, 'title').to_s
+        ended_at_value = event_schedule_doc.css('.dtend').css('.value-title').first.try(:attr, 'title').to_s
+        place_info_doc = detail_page_doc.css('.event_place_area')
+        place_geo_doc = place_info_doc.css('.geo')
         connpass_event.merge_event_attributes(
           attrs: {
             state: :active,
@@ -82,27 +89,29 @@ module ConnpassOperation
             title: event_url_doc.try(:text).to_s,
             description: Sanitizer.basic_sanitize(detail_page_doc.css("#editor_area").children.to_html.strip.to_s),
             limit_number: limit_number,
-            address: api_response_hash['address'],
-            place: api_response_hash['place'].to_s,
-            lat: api_response_hash['lat'],
-            lon: api_response_hash['lon'],
+            address: place_info_doc.css('.adr').css('.value-title').first.try(:attr, 'title'),
+            place: place_info_doc.css(".place_name").text.strip,
+            lat: place_geo_doc.css(".latitude").css(".value-title").first.try(:attr, 'title'),
+            lon: place_geo_doc.css(".longitude").css(".value-title").first.try(:attr, 'title'),
             cost: 0,
             max_prize: 0,
             currency_unit: 'JPY',
-            owner_id: api_response_hash['owner_id'],
-            owner_nickname: api_response_hash['owner_nickname'],
-            owner_name: api_response_hash['owner_display_name'],
+            owner_nickname: owner_nickname,
+            owner_name: owner_name,
             attend_number: attend_number.to_i,
             substitute_number: substitute_number,
-            started_at: api_response_hash['started_at'],
-            ended_at: api_response_hash['ended_at'],
+            started_at: Time.parse(start_at_value),
+            ended_at: Time.parse(ended_at_value),
           },
         )
-=end
-        connpass_event.save!
-        hashtags = detail_page_doc.css(".label_hashtag").map{|hashtag_dom| hashtag_dom.css("a").map{|a_tag| a_tag.text.strip } }.flatten
-        connpass_event.import_hashtags!(hashtag_strings: hashtags)
+        Event.transaction do
+          connpass_event.save!
+          hashtags = detail_page_doc.css(".label_hashtag").map{|hashtag_dom| hashtag_dom.css("a").map{|a_tag| a_tag.text.strip } }.flatten
+          connpass_event.import_hashtags!(hashtag_strings: hashtags)
+        end
+        sleep 1
       end
+      sleep 1
     end
   end
 
