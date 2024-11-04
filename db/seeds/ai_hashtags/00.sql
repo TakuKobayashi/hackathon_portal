9cba6fc37e9a4c6e6df8cd724d9e904ef0d456f5
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-11-04 04:08:54 +0000

    add 2024-11-04-04:08:54 crawl-and-publish

--- db/seeds/ai_hashtags/00.sql
+++ db/seeds/ai_hashtags/00.sql
@@ -4885,3 +4885,4 @@ INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (4910,'HopefulRomanticGameJam
 INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (4911,'GBKitbashJam1');
 INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (4912,'FediJam');
 INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (4913,'FarmFiesta2024');
+INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (4914,'Electriczinejamwinter2024');
