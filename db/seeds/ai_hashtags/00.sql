a43ef7b577b7c9cf376c82e4c081acbcd580e969
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-02-06 04:10:31 +0000

    add 2025-02-06-04:10:31 crawl-and-publish

--- db/seeds/ai_hashtags/00.sql
+++ db/seeds/ai_hashtags/00.sql
@@ -5099,3 +5099,7 @@ INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (5124,'GirlyGameJam');
 INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (5125,'GDODJam');
 INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (5126,'2DHorrorGameJam');
 INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (5127,'TokyoGameJam');
+INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (5128,'LangChainJP');
+INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (5129,'imad_mokumoku');
+INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (5130,'spacewizardjam');
+INSERT INTO `ai_hashtags` (`id`, `hashtag`) VALUES (5131,'ReverseHorrorJam');
